dd77af642729e87ae0085d05dd0262b266acffac
Vadim.Turetskiy <fromturkeywithlove@gmail.com>
2019-11-28 14:33:04 +0300

    Добален конверт .xls в .xlsx и комментарии

--- Time Sheet Constructor/Model/EmpoyeeIDParser.cs
+++ Time Sheet Constructor/Model/EmpoyeeIDParser.cs
@@ -10,23 +10,76 @@ using OfficeOpenXml.FormulaParsing.Excel.Functions.RefAndLookup;
 using OfficeOpenXml.FormulaParsing.Excel.Functions.Text;
 
 
+
 namespace Time_Sheet_Constructor.Model
 {
+    /// <summary>
+    /// Парсер отчества и табельных номеров
+    /// </summary>
     public static class EmpoyeeIDParser
     {
-        static string EmployeeFilePath =
-            @"C:\Users\vadim.turetskiy\Documents\Табель\Time sheet constructor\Список от 15.11.xlsx";
+        /// <summary>
+        /// Путь к .xls файлу со списком сотрудников
+        /// </summary>
+        /// 
+        static string EmployeeFilePath_xls =
+            @"C:\Users\vadim.turetskiy\Documents\Табель\Time sheet constructor\Список от 15.11.xls";
 
-        static FileInfo Fi => new FileInfo(EmployeeFilePath);
-        static ExcelPackage Excel => new ExcelPackage(Fi);
+        /// <summary>
+        /// Данные файла
+        /// </summary>
+        static FileInfo Fi_xls => new FileInfo(EmployeeFilePath_xls);
 
+        /// <summary>
+        /// Путь к .xlsx файлу со списком сотрудников
+        /// </summary>
+        static string EmployeeFilePath_xlsx = ConvertXLS_XLSX(Fi_xls);
+
+        /// <summary>
+        /// Данные файла
+        /// </summary>
+        static FileInfo Fi_xlsx => new FileInfo(EmployeeFilePath_xlsx);
+
+
+        static ExcelPackage Excel => new ExcelPackage(Fi_xlsx);
+
+        /// <summary>
+        /// Номер первой строки с ФИО
+        /// </summary>
         static int FirstFIORow => GetEmployeeCellRow() + 1;
-        static int FirstFIOColumn => GetEmployeeCellColumn();
-        static int LastFIORow => GetLastFIORow();
-        static int FirstIDRow => FirstFIORow;
-        static int FirstIDColumn => FirstFIOColumn + 1;
-        static int LastIDRow => LastFIORow;
 
+        /// <summary>
+        /// Номер столбца с ФИО
+        /// </summary>
+        static int FirstFIOColumn => GetEmployeeCellColumn();
+
+        /// <summary>
+        /// Номер последней строки с ФИО
+        /// </summary>
+        static int LastFIORow => GetLastFIORow();
+
+        /// <summary>
+        /// Конвертер .xls в .xlsx
+        /// </summary>
+        /// <param name="file"></param>
+        /// <returns></returns>
+        private static string ConvertXLS_XLSX(FileInfo file)
+        {
+            var app = new Microsoft.Office.Interop.Excel.Application();
+            var xlsFile = file.FullName;
+            var wb = app.Workbooks.Open(xlsFile);
+            var xlsxFile = xlsFile + "x";
+            wb.SaveAs(Filename: xlsxFile, FileFormat: Microsoft.Office.Interop.Excel.XlFileFormat.xlOpenXMLWorkbook);
+            wb.Close();
+            app.Quit();
+            return xlsxFile;
+        }
+
+        /// <summary>
+        /// Добавление отчества и табельного номера
+        /// </summary>
+        /// <param name="persons"></param>
+        /// <returns></returns>
         public static List<Person> Parse(List<Person> persons)
         {
             var personsWithId = GetPersons();
@@ -47,6 +100,10 @@ namespace Time_Sheet_Constructor.Model
             return persons;
         }
 
+        /// <summary>
+        /// Получение ФИО и табельных номеров
+        /// </summary>
+        /// <returns></returns>
         private static List<Person> GetPersons()
         {
             var persons = new List<Person>();
@@ -69,11 +126,20 @@ namespace Time_Sheet_Constructor.Model
             return persons;
         }
 
+        /// <summary>
+        /// Получение номера последней строки с ФИО
+        /// </summary>
+        /// <returns></returns>
         private static int GetLastFIORow()
         {
             return Excel.Workbook.Worksheets[1].Dimension.End.Row;
         }
 
+
+        /// <summary>
+        /// Получение номера строки ячейки "Сотрудник"
+        /// </summary>
+        /// <returns></returns>
         private static int GetEmployeeCellRow()
         {
             var searchword = "Сотрудник";
@@ -94,6 +160,10 @@ namespace Time_Sheet_Constructor.Model
             return address;
         }
 
+        /// <summary>
+        /// Получение номера столбца ячейки "Сотрудник"
+        /// </summary>
+        /// <returns></returns>
         private static int GetEmployeeCellColumn()
         {
             var searchword = "Сотрудник";
